60bccaf098d1ad5c5ad52b18a0089dae45777a39
”ShiZuoye” <albertethon@163.com>
2022-05-28 15:41:06 +0800

    NJU-ProjectN/nvboard master initialized without tracing
    NJU-ProjectN/nvboard f42b05e1f03e369cfc6701efc8737cda972d2a24 keyboard: fix spelling mistake, fix #9

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <verilated.h>
+#include "Vtop.h"
 
-int main() {
-  printf("Hello, ysyx!\n");
+int main(int argc, char** argv, char** env) {
+  
+  Verilated::mkdir("logs");
+  const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
+  contextp->debug(0);
+  contextp->randReset(2);
+  contextp->traceEverOn(true);
+  contextp->commandArgs(argc,argv);
+
+  const std::unique_ptr<Vtop> top{new Vtop{contextp.get(),"TOP"}};
+
+  while(!contextp->gotFinish()){
+    contextp->timeInc(1);
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+    assert(top->f == a ^ b);
+  }
+  top->final();
+#if VM_COVERAGE
+  Verilated::mkdir("logs");
+  contextp->coveragep()->write("logs/coverage.dat");
+#endif
   return 0;
 }
