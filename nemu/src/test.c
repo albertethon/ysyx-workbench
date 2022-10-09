702b6f346802722d60b348b7c30561ee67850d7e
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-09 16:47:55 +0800

    > compile NEMU ysyx_22041365 施佐烨 Linux wissy-MS-7C82 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:47:55 up 32 days,  6:37,  4 users,  load average: 0.58, 1.25, 1.31

--- nemu/src/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-    int main() { 
-    // unsigned long result = 2102888228u/198459428u*842275583u-1684169028u+(885030075u)*1023972905u; 
-    unsigned long  result = 877561283lu-(1529668059lu+1566589523lu /262575437lu    );
-    printf("%lu", result); 
-    return 0; 
-};
