ca76c8d13cc15a6434220cd167ecc2c089a2694c
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-25 15:47:29 +0800

    > compile NEMU ysyx_22041365 施佐烨 Linux labhost 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:47:29 up 22 days, 22:26,  6 users,  load average: 0.41, 0.18, 0.16

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -41,13 +41,12 @@ static word_t immJ(uint32_t i) { return (BITS(i,31,31) << 20) | (SEXT(BITS(i, 30
                                         (BITS(i,20,20) << 11) | (SEXT(BITS(i, 19, 12), 8) << 12); }
 static word_t immB(uint32_t i) { 
   printf("BITS(i,31,31) << 12 = %llx\n",BITS(i,31,31) << 12);
-  printf("SEXT(BITS(i, 30, 25), 6) << 5 = %lx\n",(SEXT(BITS(i, 30, 25), 6) << 5));
-  printf("SEXT(BITS(i, 11, 8), 4) << 1 = %lx\n",(SEXT(BITS(i, 11, 8), 4) << 1));
+  printf("BITS(i, 30, 25) << 5 = %lx\n",(BITS(i, 30, 25) << 5));
+  printf("BITS(i, 11, 8) << 1 = %lx\n",(BITS(i, 11, 8) << 1));
   printf("BITS(i, 7, 7) << 11 = %llx\n",(BITS(i, 7, 7) << 11));
-  printf("BITS(i,31,31) << 12 = %llx\n",(BITS(i,31,31) << 12));
 
-  return (BITS(i,31,31) << 12) | (SEXT(BITS(i, 30, 25), 6) << 5) |\
-                                        (SEXT(BITS(i, 11, 8), 4) << 1) | (BITS(i, 7, 7) << 11); }
+  return (BITS(i,31,31) << 12) | (BITS(i, 30, 25), 6) << 5 |\
+                                        (BITS(i, 11, 8), 4) << 1 | (BITS(i, 7, 7) << 11); }
 
 static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
   uint32_t i = s->isa.inst.val;
